569c1c0a414fa35c0bbf4da289be803ef90c7019
Ravindra Yadav <137895306+Ryadav0654@users.noreply.github.com>
2025-03-24 23:33:22 +0530

    Update auth-controller.ts

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -126,6 +126,8 @@ const signinController = asyncHandler(async (req: Request, res: Response) => {
     const options = {
       httpOnly: true,
       secure: true,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000,
     };
     return res
       .status(200)
